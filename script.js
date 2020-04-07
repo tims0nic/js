e7100dbe1f0395bb3d45ea1067ec1eb86d0b283d
Timur <ookami.yon@gmail.com>
2020-04-07 17:00:12 +0600

    cicles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 'use strict';
 
-let num = 50;
+
 
 if (num < 49) {
     console.log("no");
@@ -29,4 +29,23 @@ switch (num) {
     default:
         console.log("problem");
         break;
+}
+
+let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
 }
